e8a680bb40a6cdebf87e54193f6f735284555115
lindsayellis22 <lindsayellis39@gmail.com>
2022-01-25 14:51:56 -0500

    add sedds.js

---
+++ seeds.js
@@ -0,0 +1,13 @@
+const mongoose = require('./connection');
+const seedData = require('./seed.json');
+const Plant = require('./models/Plant');
+
+Plant.deleteMany({})
+	.then(() => {
+		Plant.insertMany(seedData).then((plant) => {
+			console.log('We have plants!🌿 🌱');
+			console.log(plant);
+			process.exit();
+		});
+	})
+	.catch((err) => console.error(err));
